4264f2ab53d468d9396ef517bd5b712289d62911
HUGO XAVIER JACOME VIERA <hugo.jacome@epn.edu.ec>
2018-05-20 19:39:22 -0500

    Grilla y Botones

--- CheckTime.DAL/PerfilDAL.cs
+++ CheckTime.DAL/PerfilDAL.cs
@@ -8,13 +8,13 @@ namespace CheckTime.DAL
 {
     public class PerfilDAL
     {
-        public static IQueryable<object> getUsuario(string nombreUsuario)
-        {
-            using (CheckTimeEntities bdd = new CheckTimeEntities())
-            {
-                var user = bdd.Perfil.Where(a => a.Usuario1 == nombreUsuario);
-                return user;
-            }
-        }
+        //public static IQueryable<object> getUsuario(string nombreUsuario)
+        //{
+        //    //using (CheckTimeEntities bdd = new CheckTimeEntities())
+        //    //{
+        //    //    var perfil = bdd.Perfil.Where(a => a.Usuario1 == nombreUsuario);
+        //    //    return perfil;
+        //    //}
+        //}
     }
 }
---
+++ CheckTimeWeb/Controllers/MaquetaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckTimeWeb.Controllers
+{
+    public class MaquetaController : Controller
+    {
+        // GET: Maqueta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
